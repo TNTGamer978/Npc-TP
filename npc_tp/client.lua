5995227d4c85ce66d91906770bc31e388c02a19a
TNT <103939392+TNTGamer978@users.noreply.github.com>
2022-04-18 23:43:44 +0200

    Add files via upload

---
+++ npc_tp/client.lua
@@ -0,0 +1,149 @@
+key_to_teleport = 38
+
+Teleport_Vehicles = true
+
+positions = {
+    --[[
+    {{Teleport1 X, Teleport1 Y, Teleport1 Z, Teleport1 Heading}, {Teleport2 X, Teleport 2Y, Teleport 2Z, Teleport2 Heading}, {Red, Green, Blue}, "Text for Teleport"}
+    ]]
+    {{-804.1055,-902.4132,17.88354, 0}, {1863.09, 3673.94, 33.1, 0},{36,237,157}, "Primer TP"}, -- Outside the Sheriff's Station
+    {{1860.72, 3661.49, 33.00, 0}, {1855.02, 3673.86, 33.3, 0},{255, 157, 0}, "Testing 2nd Teleport"},
+}
+
+-----------------------------------------------------------------------------
+-------------------------DO NOT EDIT BELOW THIS LINE-------------------------
+-----------------------------------------------------------------------------
+
+local player = PlayerPedId()
+
+Citizen.CreateThread(function ()
+    while true do
+        Citizen.Wait(5)
+        local player = GetPlayerPed(-1)
+        local playerLoc = GetEntityCoords(player)
+
+        for _,location in ipairs(positions) do
+            teleport_text = location[4]
+            loc1 = {
+                x=location[1][1],
+                y=location[1][2],
+                z=location[1][3],
+                heading=location[1][4]
+            }
+            loc2 = {
+                x=location[2][1],
+                y=location[2][2],
+                z=location[2][3],
+                heading=location[2][4]
+            }
+            Red = location[3][1]
+            Green = location[3][2]
+            Blue = location[3][3]
+
+            DrawMarker(1, loc1.x, loc1.y, loc1.z, 0, 0, 0, 0, 0, 0, 1.501, 1.5001, 0.5001, Red, Green, Blue, 200, 0, 0, 0, 0)
+            DrawMarker(1, loc2.x, loc2.y, loc2.z, 0, 0, 0, 0, 0, 0, 1.501, 1.5001, 0.5001, Red, Green, Blue, 200, 0, 0, 0, 0)
+
+            if CheckPos(playerLoc.x, playerLoc.y, playerLoc.z, loc1.x, loc1.y, loc1.z, 2) then 
+                alert(teleport_text)
+                
+                if IsControlJustReleased(1, key_to_teleport) then
+                    if IsPedInAnyVehicle(player, true) and Teleport_Vehicles == true then
+                        SetEntityCoords(GetVehiclePedIsUsing(player), loc2.x, loc2.y, loc2.z)
+                        SetEntityHeading(GetVehiclePedIsUsing(player), loc2.heading)
+                    else
+                        SetEntityCoords(player, loc2.x, loc2.y, loc2.z)
+                        SetEntityHeading(player, loc2.heading)
+                    end
+                end
+
+            elseif CheckPos(playerLoc.x, playerLoc.y, playerLoc.z, loc2.x, loc2.y, loc2.z, 2) then
+                alert(teleport_text)
+
+                if IsControlJustReleased(1, key_to_teleport) then
+                    if IsPedInAnyVehicle(player, true) and Teleport_Vehicles == true then
+                        SetEntityCoords(GetVehiclePedIsUsing(player), loc1.x, loc1.y, loc1.z)
+                        SetEntityHeading(GetVehiclePedIsUsing(player), loc1.heading)
+                    else
+                        SetEntityCoords(player, loc1.x, loc1.y, loc1.z)
+                        SetEntityHeading(player, loc1.heading)
+                    end
+                end
+            end            
+        end
+    end
+end)
+
+function CheckPos(x, y, z, cx, cy, cz, radius)
+    local t1 = x - cx
+    local t12 = t1^2
+
+    local t2 = y-cy
+    local t21 = t2^2
+
+    local t3 = z - cz
+    local t31 = t3^2
+
+    return (t12 + t21 + t31) <= radius^2
+end
+
+function alert(msg)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(msg)
+    DisplayHelpTextFromStringLabel(0,0,1,-1)
+end
+
+-- Punto de spawn de NPC
+
+local coordonate = {
+    {-804.1055,-902.4132,17.88354,"tp a la playa",500.77,0xC99F21C4,"a_m_y_business_01"},
+    {441.06457519531,-978.93707275391,29.689584732056,"Agent Politie",535.77,0x15F8700D,"s_f_y_cop_01"},
+    {446.60791015625,-988.55383300781,29.689584732056,"Garda Politie",370.77,0x56C96FC6,"s_m_m_prisguard_01"},
+    {-1570.9993896484,-574.92303466797,107.52293395996,"Registru comertului",381.77,0xC99F21C4,"a_m_y_business_01"}
+}
+
+Citizen.CreateThread(function()
+
+    for _,v in pairs(coordonate) do
+      RequestModel(GetHashKey(v[7]))
+      while not HasModelLoaded(GetHashKey(v[7])) do
+        Wait(1)
+      end
+  
+      RequestAnimDict("mini@strip_club@idles@bouncer@base")
+      while not HasAnimDictLoaded("mini@strip_club@idles@bouncer@base") do
+        Wait(1)
+      end
+      ped =  CreatePed(4, v[6],v[1],v[2],v[3], 3374176, false, true)
+      SetEntityHeading(ped, v[5])
+      FreezeEntityPosition(ped, true)
+      SetEntityInvincible(ped, true)
+      SetBlockingOfNonTemporaryEvents(ped, true)
+      TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+    end
+end)
+
+function DrawText3D(x,y,z, text, scl, font) 
+
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+ 
+    local scale = (1/dist)*scl
+    local fov = (1/GetGameplayCamFov())*100
+    local scale = scale*fov
+   
+    if onScreen then
+        SetTextScale(0.0*scale, 1.1*scale)
+        SetTextFont(font)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 255)
+        SetTextDropshadow(0, 0, 0, 0, 255)
+        SetTextEdge(2, 0, 0, 0, 150)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x,_y)
+    end
+end
